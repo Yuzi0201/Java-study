7b222518dbc2eae564e40dce3edd4eee832e0791
Yuzi <2205816944@qq.com>
2021-10-31 14:17:36 +0800

    experiment1_question1

---
+++ src/Yuzi/experiment1/q1/main.java
@@ -0,0 +1,32 @@
+package Yuzi.experiment1.q1;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class main {
+    public static void main(String[] args) {
+        while (true) {
+            Random r = new Random();
+            int i = r.nextInt(100) + 1;
+            Scanner s = new Scanner(System.in).useLocale(Locale.US);
+            System.out.print("请输入一个1-100之间的数： ");
+            int cin = s.nextInt();
+            int counter = 1;
+            while (cin != i) {
+                if (cin > i)
+                    System.out.print("猜大了！再猜： ");
+                else
+                    System.out.print("猜小了！再猜： ");
+                counter++;
+                cin = s.nextInt();
+            }
+            System.out.println("猜对了！你一共猜了" + counter + "次！");
+            System.out.println("输入exit退出，其他键重玩");
+            String restart=s.next();
+            if (restart.equals("exit"))
+                System.exit(0);
+        }
+    }
+}
---
+++ src/Yuzi/experiment1/q2/main.java
@@ -0,0 +1,12 @@
+package Yuzi.experiment1.q2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class main {
+    public static void main(String[] args) {
+
+
+    }
+}
